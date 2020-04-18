857b2566e159cd3f62074de6b77177f5365e16fd
Dmitry Yv <df.creative@gmail.com>
2020-04-18 14:32:58 -0400

    Opinionize formatting

--- libs/spect.js
+++ libs/spect.js
@@ -3,21 +3,17 @@ module.exports = function merge (parent, a, b) {
   let i, cur = a[0], next, bi
 
   for (i = 0; i <= b.length; i++) {
-    bi = b[i]
-    next = cur && cur.nextSibling
+    bi = b[i], next = cur && cur.nextSibling
 
     // skip
     if (cur === bi) cur = next
 
     // remove
-    else if (cur && !bidx.has(cur)) {
-      parent.removeChild(cur), i--, cur = next
-    }
+    else if (cur && !bidx.has(cur)) (parent.removeChild(cur), i--, cur = next)
+
     else if (bi) {
-      // swap (only 1:1 pairs)
-      if (aidx.has(bi) && b[i+1] === next) {
-        parent.insertBefore(cur, bi), cur = next
-      }
+      // swap (only 1:1 pairs) (technically redundant but avoids long rearrangements)
+      if (aidx.has(bi) && b[i+1] === next) (parent.insertBefore(cur, bi), cur = next)
 
       // insert after bi-1, bi
       parent.insertBefore(bi, cur)
