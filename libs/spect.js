0294606f37d62f39be6b709e42db516b74349629
Dmitry Yv <df.creative@gmail.com>
2020-05-05 00:49:32 -0400

    Add 191 bytes implementation

--- libs/spect.js
+++ libs/spect.js
@@ -1,23 +1,22 @@
 module.exports = (parent, a, b, end = null) => {
-  let bidx = new Set(b), aidx = new Set(a), i = 0, cur = a[0] || end, next, bi
+  let bidx = new Set(b), aidx = new Set, i = 0, cur, next, bi
+  
+  while (bi = a[i++]) !bidx.has(bi) ? parent.removeChild(bi) : (aidx.add(bi), cur = cur || bi)
+  cur = cur || end, i = 0
 
-  while ((bi = b[i++]) || cur != end) {
-    next = cur ? cur.nextSibling : end
+  while (bi = b[i++]) {
+    next = cur.nextSibling
 
     // skip
     if (cur == bi) cur = next
 
-    // insert has higher priority, inc. tail-append shortcut
-    else if (bi && (cur == end || bidx.has(cur))) {
+    else {
       // swap
       if (b[i] === next && aidx.has(bi)) cur = next
 
       // insert
       parent.insertBefore(bi, cur)
     }
-
-    // remove
-    else (parent.removeChild(cur), cur = next, i--)
   }
 
   return b
