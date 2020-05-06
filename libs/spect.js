242918903d703099d1c6155902f660fe697bffc6
Dmitry Yv <df.creative@gmail.com>
2020-05-06 10:38:38 -0400

    Loose comparisons

--- libs/spect.js
+++ libs/spect.js
@@ -2,10 +2,10 @@ module.exports = (parent, a, b, end = null) => {
   let i = 0, cur, next, bi, n = b.length, dif = n - a.length
 
   // skip head
-  while (a[i] === b[i] && i++ < n);
+  while (a[i] == b[i] && i++ < n);
 
   // skip tail
-  while (--n >= dif && b[n] === a[n - dif]) end = b[n];
+  while (--n >= dif && b[n] == a[n - dif]) end = b[n];
 
   // append/prepend shortcut
   if (i > n - dif) while (i <= n) parent.insertBefore(b[i++], end)
@@ -19,14 +19,14 @@ module.exports = (parent, a, b, end = null) => {
       if (cur == bi) cur = next
 
       // swap / replace
-      else if (i < n && b[i] === next) (parent.replaceChild(bi, cur), cur = next)
+      else if (i < n && b[i] == next) (parent.replaceChild(bi, cur), cur = next)
 
       // insert
       else parent.insertBefore(bi, cur)
     }
 
     // remove tail
-    while (cur !== end) (next = cur.nextSibling, parent.removeChild(cur), cur = next)
+    while (cur != end) (next = cur.nextSibling, parent.removeChild(cur), cur = next)
   }
 
   return b
